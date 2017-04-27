926f661b2c2380b24f809f69ea69384ad460c9e2
Vibhu Gupta <gupta.vibhu1@gmail.com>
2017-04-27 23:17:06 +0530

    New Booking Code Starting

--- src/test/java/demo/SeleniumCode.java
+++ src/test/java/demo/SeleniumCode.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,10 +17,29 @@ import java.util.concurrent.TimeUnit;
 public class SeleniumCode {
 
     public static WebDriver driver = null;
-    public static String date;
+    public static String startingDate;
+    public static String[] skillNameOtherThanPPT = new String[21];
+    int currentTimeVariable;
+    int timeSelectorXpathVariable=0;
+    String completeTimeSelectorXpathVariable="";
     String chromeDriverFileLocation = System.getProperty("user.dir") + "\\src\\resources\\chromedriver\\chromedriver.exe";
 
-    public void loginVAApp(){
+    public String currentDate() {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateobj = new Date();
+        // System.out.println(df.format(dateobj));
+        return (df.format(dateobj));
+    }
+
+    public String currentTimeInHours() {
+        // DateFormat df = new SimpleDateFormat("HH:mm:ss");
+        DateFormat df = new SimpleDateFormat("HH");
+        Date dateobj = new Date();
+        //  System.out.println(df.format(dateobj));
+        return (df.format(dateobj));
+    }
+
+    public void loginVAApp() {
 
         System.setProperty("webdriver.chrome.driver", chromeDriverFileLocation);
         driver = new ChromeDriver();
@@ -29,22 +51,24 @@ public class SeleniumCode {
         driver.findElement(By.xpath("//button[@type='submit']")).click();
     }
 
-    public void sleep(int sleepValue){
+    public void sleep(int sleepValue) {
         try {
             Thread.sleep(sleepValue);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    public void homeVAApp(){
+
+    public void homeVAApp() {
         sleep(1000);
         driver.findElement(By.xpath("//div[@class='selectProfile container']")).click();
+        sleep(1000);
         driver.findElement(By.xpath("//input[@id='chargeCodeInputBox']")).sendKeys("CCC123");
         driver.findElement(By.xpath("//a[@class='modal-action modal-close waves-effect waves-green']")).click();
         sleep(5000);
         WebElement element = driver.findElement(By.xpath("/html/body/section/ui-view/div[1]/section/div[5]/div[1]/select"));
 
-        Select selectObj =new Select(element);
+        Select selectObj = new Select(element);
         String toSelectValue = "string:";
 
         selectObj.selectByValue(toSelectValue.concat("CCC123"));
@@ -52,9 +76,41 @@ public class SeleniumCode {
         driver.findElement(By.xpath("//button[@type='submit']")).click();
     }
 
-    public void newBooking(){
+    public void newBooking() {
+
         sleep(1000);
-        date = driver.findElement(By.xpath("/html/body/section/ui-view/div[1]/section/form/div/div[2]/div[2]/div/span")).getText();
+        driver.findElement(By.xpath("//div[@id='newBooking']")).click();
+        sleep(2000);
+        startingDate = driver.findElement(By.xpath("//select[@id='selectDate']/option[2]")).getText();
+        System.out.println("startingDate :" + startingDate);
+
+        // To Validate Other Picklist values Code Starting
+
+        driver.findElement(By.xpath("//div[@id='enableOthers']")).click();
+        sleep(2000);
+        for (int i = 1; i < 21; i++) {
+            skillNameOtherThanPPT[i] = driver.findElement(By.xpath("//ul[@class='dropdown']/li[" + i + "]")).getText();
+            //  System.out.println(skill[i]);
+        }
+
+        //To Validate Other Picklist values Code Ending
+
+        //To select High End PPT
+        driver.findElement(By.xpath("//div[@class='container-fluid']/div[1]/div[2]/div[1]")).click();
+
+        driver.findElement(By.xpath("//input[@id='startTime']")).click();
+        currentTimeVariable = Integer.parseInt(currentTimeInHours());
+        // currentTimeInHours();
+        System.out.println("currentTimeVariable : " + currentTimeVariable);
+        if(currentTimeVariable<24){
+            timeSelectorXpathVariable=timeSelectorXpathVariable+1;
+        }
+        completeTimeSelectorXpathVariable =completeTimeSelectorXpathVariable
+                .concat("//div[@class='clockpicker-plate']/div[2]/div[")
+                .concat(Integer.toOctalString(timeSelectorXpathVariable)).concat("]");
+        driver.findElement(By.xpath(completeTimeSelectorXpathVariable)).click();
+        driver.findElement(By.xpath("//button[@type='button']")).click();
+
     }
 }
 
--- src/test/java/demo/Test1.java
+++ src/test/java/demo/Test1.java
@@ -10,6 +10,8 @@ import org.testng.annotations.Test;
 import io.restassured.response.Response;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Test1 {
 
@@ -20,7 +22,7 @@ public class Test1 {
     @BeforeSuite
     public void login() {
 
-     //   scObj.loginVAApp();
+        scObj.loginVAApp();
 
         given().
                 header("Authorization", "Basic Z3Vlc3RfaG91c2VfcHdkOmdoX3NlY3JldA==").
@@ -46,12 +48,12 @@ public class Test1 {
         System.out.println(access_token);
     }
 
-    @Test (priority = 1)
+    @Test(priority = 1)
     public void home() {
         access_token = "Bearer ".concat(access_token);
         System.out.println("access_token : " + access_token);
 
-     //   scObj.homeVAApp();
+        scObj.homeVAApp();
 
         given().
                 header("Authorization", access_token).
@@ -79,28 +81,101 @@ public class Test1 {
                 body("fmnoId", equalTo("123"));
     }
 
-    @Test (priority = 2)
-    public void NewBooking(){
+    @Test(priority = 2)
+    public void NewBooking() {
 
-         // Date
+        scObj.newBooking();
+        // Date Logic Starting
         // To store the response and date comparision
-        Response r =
+        Response responseStartDate =
                 given().header("Authorization", access_token).
                         when().
                         get("http://application-721436538.ap-northeast-2.elb.amazonaws.com/va/booking/dates/open-for-booking").
                         then().extract().response();
 
 
-        int statusValue = r.path("status");
-        String messageValue = r.path("message");
-        ArrayList<String> date = r.path("datesOpenForBooking");
-        System.out.println("statusValue : " + statusValue);
-        System.out.println("date : " + date);
-        System.out.println("date.size : " + date.size());
-        if (date.size() < 1) {
+        ArrayList<String> startingDate = responseStartDate.path("datesOpenForBooking");
 
-            Assert.assertEquals(SeleniumCode.date, "Sorry, currently no dates are open for booking.");
+        System.out.println("Starting Date First Value : " + startingDate.get(0));
+        System.out.println("date.size : " + startingDate.size());
+
+        String startingDateObj = scObj.currentDate();
+
+
+        if (startingDate.size() < 1) {
+
+            Assert.assertEquals(SeleniumCode.startingDate, null);
+
+        } else {
+            Assert.assertEquals(startingDate.get(0), startingDateObj);
+            Assert.assertEquals(startingDate.get(0), SeleniumCode.startingDate);
         }
+
+        // Date Logic Ending
+
+        // Charge Code List Starting
+
+        given().
+                header("Authorization", access_token).
+                get("http://application-721436538.ap-northeast-2.elb.amazonaws.com/profile/chargeCode/list").
+                then().
+                statusCode(200).
+                body("message", equalTo("Successfully listed all the charge codes!"));
+
+
+        // Charge Code List Ending
+
+        // HighEnd PPt Code Starting
+        Response responseHighEndPPT =
+                given().
+                        header("Authorization", access_token).
+                        get("http://application-721436538.ap-northeast-2.elb.amazonaws.com/va/skill/ppt").
+                        then().
+                        statusCode(200).
+                        extract().response();
+        String highEndPPTText = responseHighEndPPT.path("skill");
+        Assert.assertEquals(highEndPPTText, "Highend PPT");
+
+        Response responseNonPPT =
+                given().
+                        header("Authorization", access_token).
+                        get("http://application-721436538.ap-northeast-2.elb.amazonaws.com/va/skill/nonppt").
+                        then().
+                        statusCode(200).
+                        extract().response();
+
+        ArrayList<String> highEndNonPPTText = responseNonPPT.path("skill");
+        int highEndNonPPTSize = highEndNonPPTText.size();
+        String[] skillNameFromAPI = new String[highEndNonPPTSize];
+      //  System.out.println("highEndNonPPTSize : " + highEndNonPPTSize);
+        for (int i = 0; i < highEndNonPPTSize - 1; i++) {
+            skillNameFromAPI[i] = highEndNonPPTText.get(i);
+            // System.out.println("highEndNonPPTText : " +s[i]);
+            Assert.assertEquals(skillNameFromAPI[i], SeleniumCode.skillNameOtherThanPPT[i + 1]);
+        }
+        //   System.out.println("highEndNonPPTText : " +highEndNonPPTText);
+
+
+        // HighEnd PPt Code Ending
+
+        // Duration Code Starting
+        Response responseDuration =
+                given().
+                        header("Authorization", access_token).
+                        get("http://application-721436538.ap-northeast-2.elb.amazonaws.com/qava/duration.json").
+                        then().
+                        statusCode(200).
+                        extract().response();
+
+        HashMap<String, String> durationHours = new HashMap<String, String>();
+        durationHours = responseDuration.path("userData");
+        System.out.println("durationHours Size: " + durationHours.size());
+        String slotDurationAPI = "";
+        for (int i = 1; i <= durationHours.size(); i++) {
+            slotDurationAPI = Integer.toString(i);
+            System.out.println("durationHours slots : " + durationHours.get(slotDurationAPI));
+        }
+
     }
 
 }
